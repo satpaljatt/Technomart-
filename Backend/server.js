78e978eb9a084f50a43d1404ae67d184e47a48f9
Satpal Singh <satpals7125@gmail.com>
2026-07-28 00:02:09 +0530

    Vercel backend settings done

--- Backend/server.js
+++ Backend/server.js
@@ -27,6 +27,8 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+// });
+
+export default app;
